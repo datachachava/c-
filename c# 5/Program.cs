42acc49e0576a0215255a33d56745ba1a8e94f5f
datachachava <datachachava2001@gmail.com>
2026-06-13 14:42:10 +0400

    Add files via upload

---
+++ c# 5/Program.cs
@@ -0,0 +1,100 @@
+﻿namespace c__5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             დავალება 1
+ შექმენით ერთ განზომილებიანი ორი მასივი.
+ შეავსეთ ორივე მასივი ელემენტებით.
+ გააერთიანე ერთ მასივში ორივე მასივის ელემენტები.
+ დაბეჭდეთ საბოლოოდ მიღებული მასივი.
+მაგალითად, თუ პირველი მასივის ელემენტების: 1 2 3
+ხოლო მეორე მასივის ელემენტებია : 4 5 6
+შედეგად უნდა მიიღოთ:
+resultArray = [1, 2, 3, 4, 5, 6]
+
+             */
+
+            //int[] numarr1 = [20, 60, 30, 40];
+
+            //int[] numarr2 = [50, 60, 70, 80];
+
+
+            //int[] arr3 = new int[numarr1.Length + numarr2.Length];
+
+            //for (int i = 0; i < numarr1.Length; i++)
+            //{
+            //    arr3[i] = numarr1[i];
+            //}
+
+            //for (int i = 0; i < numarr2.Length; i++)
+            //{
+            //    arr3[numarr1.Length + i] = numarr2[i];
+            //}
+
+
+            //foreach (var item in arr3)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+
+            //			დავალება 2
+            //შექმენით ინტების მასივი და შეავსეთ ელემენტებით. მაგ: 3, 5, -4, 8, 11, 1, -1, 6
+            //კონსოლიდან გადმოეცით რაღაც რიცხვი რომელსაც შეინახავთ targetSum ცვლადში.
+            //მოძებნეთ მასივში ყველა ის ორი ელემენტი რომლის ჯამიც იქნება targetSum ტოლი და ამწყვილებისგან შექმენით მასივი.
+            //დააბრუნეთ ამ ელემენტების წყვილები კონსოლში.
+            //2 | P a g e
+            //მაგალითად: array = { 3, 5, -4, 8, 11, 1, -1, 6}
+            //			targetSum = 7
+            //შედეგად უნდა მივიღოთ: resultArray = [[1, 6], [8, -1], [-4, 11]]
+
+
+
+
+            //int[] numbersarr = [3, 5, -4, 8, 11, 1, -1, 6, 3, 8, -6];
+
+            //bool isValid = int.TryParse(Console.ReadLine(), out int targetSum);  // 7
+
+            //int count = 0;
+
+
+            //for (int i = 0; i < numbersarr.Length; i++)
+            //{
+            //    for (int j = i + 1; j < numbersarr.Length; j++)
+            //    {
+            //        if (numbersarr[i] + numbersarr[j] == targetSum)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //}
+
+
+            //int[][] resultArray = new int[count][];
+
+            //int iIndex = 0;
+
+            //for (int i = 0; i < numbersarr.Length; i++)
+            //{
+
+
+            //   for (int j = i + 1; j < numbersarr.Length; j++)
+            //    {
+
+
+            //      if (numbersarr[i] + numbersarr[j] == targetSum)
+            //        {
+            //            resultArray[iIndex] = [numbersarr[i], numbersarr[j]];
+            //            iIndex++;
+            //        }
+            //    }
+            //}
+
+
+
+        }
+    }
+}
