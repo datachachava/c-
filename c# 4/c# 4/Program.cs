c50f3b97e5d2b9fd027f4ddcd570c6486bcd0030
datachachava <datachachava2001@gmail.com>
2026-06-13 14:27:14 +0400

    Add files via upload

---
+++ c# 4/c# 4/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace c__4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             დავალება 1
+გააკეთე კონსოლიდან შემოყვანილი რიცხვის გამრავლების ტაბულის ერთი ბლოკი. (ათის ნამრავლის
+ჩათვლით)
+შესაყვანი სატესტო მონაცემი: 13
+მოსალოდნელი შედეგი:
+13 * 1 = 13
+13 * 2 = 26
+.........
+13 * 10 = 130
+
+             */
+
+            //Console.WriteLine("enter a number");
+            //string numStr = Console.ReadLine();
+
+            //bool isValid = int.TryParse(numStr, out int num);
+
+            //if (isValid)
+            //{
+            //    for (int i = 0; i <= 10; i += 2)
+            //    {
+            //        Console.WriteLine($" {num} * {i} = {num * i}");
+            //    }
+            //}
+
+            /*
+             დავალება 2
+დაწერეთ პროგრამა რომელიც გამოიტანს კონსოლში ფიფქებით შედგენილ პირამიდის ფორმას. მაგალითად
+ციფრი 4–ის შეყვანისას კონსოლში გამოვა შემდეგი სახის პირამიდა:
+ *
+* *
+* * *
+* * * * 
+             */
+
+            //Console.WriteLine("enter a number");
+            //string amountStr = Console.ReadLine();
+
+            //bool isValid = int.TryParse(amountStr, out int lineAmount);
+
+            //for (int i = 1; i <= lineAmount; i++) 
+            //{
+
+            //    for (int j = 1; j <= lineAmount - i; j++)
+            //    {
+            //        Console.Write(" ");
+            //    }
+
+            //    for (int a = 1; a <= i; a++)
+            //    {
+            //        Console.Write(" *");
+            //    }
+
+
+            //    Console.WriteLine();
+            //}
+
+            /*
+             დავალება 3
+დაწერეთ პროგრამა რომელიც კონსოლიდან წაკითხულ რიცხვამდე დააჯამებს ყველა ლუწ რიცხვს და პასუხი
+გამოიტანეთ კონსოლში
+ 
+             
+             */
+
+            //Console.WriteLine("enter a number");
+
+            //bool isValid = int.TryParse(Console.ReadLine(), out int num);
+
+            //int sum = 0;
+
+            //for (int i = 0; i < num; i += 2)
+            //{
+            //    sum += i;
+            //    if (i % 2 == 0)
+            //    {
+            //        sum += i;
+            //    }
+            //}
+
+        }
+    }
+}
